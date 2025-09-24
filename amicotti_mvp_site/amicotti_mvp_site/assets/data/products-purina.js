d7421ad278259084a5be326d791bb3b733390787
renanchavezavila <renanchavezavila@gmail.com>
2025-09-24 15:56:17 -0500

    Create products-purina.js

---
+++ amicotti_mvp_site/amicotti_mvp_site/assets/data/products-purina.js
@@ -0,0 +1,26 @@
+// products-purina.js
+const PRODUCTS = [
+  {
+    id: "purina-cat-adulto",
+    brand: "purina",
+    name: "Cat Chow Adulto",
+    species: "gato",
+    image: "https://TUHOST/imagenes/purina/cat-chow-adulto-1_5kg.jpg",
+    tags: ["adulto"],
+    variants: [
+      { label: "500 g",  price: 7.20 },
+      { label: "1.5 kg", price: 19.50 }
+    ]
+  },
+  {
+    id: "purina-dog-adultos",
+    brand: "purina",
+    name: "Dog Chow Adultos",
+    species: "perro",
+    image: "https://TUHOST/imagenes/purina/dog-chow-adultos-8kg.jpg",
+    tags: ["adulto"],
+    variants: [
+      { label: "8 kg",   price: 32.80 }
+    ]
+  }
+];
