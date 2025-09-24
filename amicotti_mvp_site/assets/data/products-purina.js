e6b2602f04218fe4f6328c811ff831d7d0f9897c
renanchavezavila <renanchavezavila@gmail.com>
2025-09-24 17:47:24 -0500

    Create products-purina.js

---
+++ amicotti_mvp_site/assets/data/products-purina.js
@@ -0,0 +1,15 @@
+// products-purina.js (prueba)
+const PRODUCTS = [
+  {
+    id: "purina-cat-chow-adulto",
+    brand: "purina",
+    name: "Cat Chow Adulto",
+    species: "gato",
+    image: "./assets/logo.png", // usa cualquier imagen que ya tengas
+    tags: ["adulto"],
+    variants: [
+      { label: "500 g",  price: 7.20 },
+      { label: "1.5 kg", price: 19.50 }
+    ]
+  }
+];
