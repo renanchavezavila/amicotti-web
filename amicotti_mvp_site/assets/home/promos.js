42292978c8d4b00d0e3c3ef7d298e8619bd187ea
renanchavezavila <renanchavezavila@gmail.com>
2025-10-01 19:36:53 -0500

    Create promos.js

---
+++ amicotti_mvp_site/assets/home/promos.js
@@ -0,0 +1,27 @@
+// assets/home/promos.js
+window.PROMOS_HOME = [
+  {
+    title: "Royal Canin Gato Urinary 1.5kg",
+    sku: "RC-URIN-15",
+    img: "assets/home/promos/promo-urinary.jpg",
+    href: "catalogo.html?marca=Royal+Canin&buscar=urinary",
+    subtitle: "Precio especial esta semana",
+    allow_cart: false
+  },
+  {
+    title: "Pro Plan Cat Sterilized 1.5kg",
+    sku: "PP-STC-15",
+    img: "assets/home/promos/promo-sterilized.jpg",
+    href: "catalogo.html?marca=Purina&buscar=sterilized",
+    subtitle: "Descuento por tiempo limitado",
+    allow_cart: true
+  },
+  {
+    title: "Felix Travesuras Ocean Mix (x12)",
+    sku: "FX-OCN-12",
+    img: "assets/home/promos/promo-felix.jpg",
+    href: "catalogo.html?marca=Purina&buscar=felix%20travesuras",
+    subtitle: "Pack ahorro x 12 sobres",
+    allow_cart: false
+  }
+];
